a931bdccdacf2f920a87e1b0de7bbd1f1c3b4e6b
Oliver Cutting <oliver.cutting@gmail.com>
2022-02-07 16:49:41 +0000

    Step 7 complete

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,5 +4,7 @@ class Bookmark
 
   def self.all
     connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url']}
   end
 end
